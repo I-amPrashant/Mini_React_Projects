caafd93f4b6bb4e1f8aaa1e80d4fb6c489c8afdf
I-amPrashant <airmax50cent@gmail.com>
2024-08-13 08:48:08 +0545

    17th commit: AddToCart navbar UI upload

---
+++ AddToCart/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='logo-wrapper' style={{fontSize: '2rem'}}>
+            <span><strong>Car</strong><strong style={{color: '#ffa31a'}}>Hub</strong></span>
+        </div>
+        <div className='nav-link'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+                <button className='navbar-button'><i class="fa-solid fa-cart-shopping"></i><span className='addedCount'>2</span></button>
+        </div>
+    </nav>
+  )
+}
