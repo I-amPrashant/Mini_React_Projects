e233a45ed6796d019501869b9ea48a698fb2b28b
I-amPrashant <airmax50cent@gmail.com>
2024-08-16 14:46:30 +0545

    23th commit: Add to cart navbar responsive css add

--- AddToCart/src/Navbar.jsx
+++ AddToCart/src/Navbar.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
 export default function Navbar({totalBookmark}) {
-  return (
+  const sidebarRef=useRef()
+
+  useEffect(()=>{
+    const handleResize=()=>{
+      sidebarRef.current.style.transform='translateX(0)'
+    }
+    window.addEventListener('resize', handleResize)
+
+    return ()=>{
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+ 
+  const handleHamburgerClick=()=>{
+    sidebarRef.current.style.transform='translateX(0)'
+  }
+  const handleCloseSidebar=()=>{
+    sidebarRef.current.style.transform='translateX(100%)'
+  }
+  return(
     <nav className='navbar'>
         <div className='logo-wrapper' style={{fontSize: '2rem'}}>
             <span><strong>Car</strong><strong style={{color: '#ffa31a'}}>Hub</strong></span>
         </div>
-        <div className='nav-link'>
+        <div className='hamburger-wrapper'>
+            <button className='hamburger' onClick={()=> handleHamburgerClick()}><i className="fa-solid fa-bars"></i></button>
+        </div>
+        <div className='nav-link' ref={sidebarRef}>
             <ul>
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
                 <button className='navbar-button'><i className="fa-solid fa-cart-shopping"></i><span className='addedCount'>{totalBookmark?totalBookmark:''}</span></button>
+            <div className='hideSidebar' onClick={()=> handleCloseSidebar()}><i className="fa-solid fa-xmark"></i></div>    
         </div>
     </nav>
   )
