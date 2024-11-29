5ef6b8c7a4932d96c27aa92a198b0f1c2564fe79
I-amPrashant <airmax50cent@gmail.com>
2024-11-29 13:37:39 +0545

    35th commit: drag n drop UI upload

--- dragAndDrop/src/App.jsx
+++ dragAndDrop/src/App.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function App() {
+
+  const [dataItems, setDataItems] = useState({
+    data1:["item 1", "item 2", "item 3"],
+    data2:["item 1", "item 2", "item 3"],
+});
+
+  const [draggedIndex, setDraggedIndex] = useState({index:'', dataContainer:''});
   return (
-    <div>Hello world </div>
+    <div className='main-container'>
+        <div className='item-container'>
+          <h2 className=''>First Data Container</h2>
+
+          <div className="items-wrapper">
+          {dataItems.data1.map((item, index)=>{
+            return (
+              <div className='item' key={index} draggable={true}>
+                  {item}
+              </div>
+            )
+          })}
+          </div>
+        </div>
+        <div className='item-container'>
+        <h2 className=''>Second Data Container</h2>
+
+          <div className="items-wrapper">
+          {dataItems.data1.map((item, index)=>{
+            return (
+              <div className='item' key={index} draggable={true}>
+                  {item}
+              </div>
+            )
+          })}
+          </div>
+        </div>
+    </div>
   )
 }
