5a51416ef32742d1791350722159dca7979f484d
I-amPrashant <airmax50cent@gmail.com>
2024-11-29 15:26:31 +0545

    36th commit: drag and drop items from one container to other and vice versa

--- dragAndDrop/src/App.jsx
+++ dragAndDrop/src/App.jsx
@@ -1,41 +1,101 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function App() {
-
   const [dataItems, setDataItems] = useState({
-    data1:["item 1", "item 2", "item 3"],
-    data2:["item 1", "item 2", "item 3"],
-});
+    data1: ["item 1", "item 2", "item 3"],
+    data2: ["item A", "item B", "item C"],
+  });
 
-  const [draggedIndex, setDraggedIndex] = useState({index:'', dataContainer:''});
+  const [draggedIndex, setDraggedIndex] = useState({
+    index: "",
+    dataContainer: "",
+  });
+
+  const handleDragStart = (index, dataContainer) => {
+    setDraggedIndex({
+      index: index,
+      dataContainer: dataContainer,
+    });
+  };
+
+  //preventing default behavior of not being able to drop into the container
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (dataContainer) => {
+    if (dataContainer === draggedIndex.dataContainer) {
+      return;
+    }
+    const newItems = [{ ...dataItems.data1, ...dataItems.data2 }];
+    const deleteContainer = draggedIndex.dataContainer;
+    const removedItemsContainerData = dataItems[`${deleteContainer}`].filter(
+      (i) => i !== dataItems[`${deleteContainer}`][draggedIndex.index]
+    );
+
+    const addedItemsContainerData = [
+      ...dataItems[`${dataContainer}`],
+      dataItems[`${deleteContainer}`][draggedIndex.index],
+    ];
+
+    setDataItems({
+      ...newItems,
+      [`${draggedIndex.dataContainer}`]: removedItemsContainerData,
+      [`${dataContainer}`]: addedItemsContainerData,
+    });
+
+    setDraggedIndex({
+      index: "",
+      dataContainer: "",
+    });
+  };
+
   return (
-    <div className='main-container'>
-        <div className='item-container'>
-          <h2 className=''>First Data Container</h2>
+    <div className="main-container">
+      <div className="item-container">
+        <h2>First Data Container</h2>
 
-          <div className="items-wrapper">
-          {dataItems.data1.map((item, index)=>{
+        <div
+          className="items-wrapper"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={() => handleDrop("data1")}
+        >
+          {dataItems.data1.map((item, index) => {
             return (
-              <div className='item' key={index} draggable={true}>
-                  {item}
+              <div
+                className="item"
+                key={index}
+                draggable={true}
+                onDragStart={() => handleDragStart(index, "data1")}
+              >
+                {item}
               </div>
-            )
+            );
           })}
-          </div>
         </div>
-        <div className='item-container'>
-        <h2 className=''>Second Data Container</h2>
+      </div>
+      <div className="item-container">
+        <h2>Second Data Container</h2>
 
-          <div className="items-wrapper">
-          {dataItems.data1.map((item, index)=>{
+        <div
+          className="items-wrapper"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={() => handleDrop("data2")}
+        >
+          {dataItems.data2.map((item, index) => {
             return (
-              <div className='item' key={index} draggable={true}>
-                  {item}
+              <div
+                className="item"
+                key={index}
+                draggable={true}
+                onDragStart={() => handleDragStart(index, "data2")}
+              >
+                {item}
               </div>
-            )
+            );
           })}
-          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
